1b946dd3d1c2f9d21fa264fe9daaae4ee92c64a9
brenonerb <115164045+brenonerb@users.noreply.github.com>
2025-07-30 18:00:54 -0300

    SEGUNDA Commit de Arte

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,5 +1,15 @@
 extends Control
 
+@onready var game_logo: Sprite2D = $GameLogo
+var target_pos: Vector2 = Vector2(578.0, 180.0)
 
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+func _ready() -> void:
+	LogoSpawn()
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("move_shoot"):
+		get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+
+func LogoSpawn():
+	var LogoMove = create_tween()
+	LogoMove.tween_property(game_logo, "position", target_pos, 2.0).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
